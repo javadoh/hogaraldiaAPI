9c57fef65188d229a55308e5e55392bacc6f5324
javadoh <javadoh@gmail.com>
2023-10-26 11:47:48 -0300

    publicidad update await async

--- controllers/publicidad.js
+++ controllers/publicidad.js
@@ -6,7 +6,7 @@ var PublicidadSchema = require('.././models/publicidad');
 mongoose.set('debug', true);
 
 //TODAS LAS PUBLICIDADES
-exports.findAll = function(req, res){
+exports.findAll = async function(req, res){
   console.log('##### FIND ALL PUBLICIDAD #####');
   var condominio_id = req.params.condominio_id;
 
@@ -28,7 +28,7 @@ exports.findAll = function(req, res){
     - Ordena las publicidades a partir de la fecha más reciente (Al vender se debe elaborar un esquema basado en las fechas para la posición del item)
     - Las publicidades con fecha fin menor al día de hoy no se muestran.
   */
-  PublicidadSchema.find(
+  const findAllPub = await PublicidadSchema.find(
   {$and: [{$or: [{'condominio_id':condominio_id },{'condominio_id':0}]}, {$expr: {$gte: 
     [
         {$dateFromString: {dateString: "$fecha_fin_publicidad", 'format':'%m-%d-%Y'}},
@@ -36,16 +36,12 @@ exports.findAll = function(req, res){
         ]
     }}
     ]}
-  ,'publicidad_id path_publicidad_imagen fecha_inicio_publicidad fecha_fin_publicidad path_publicidad_webpage tipo titulo descripcion condominio_id',function(err, results){
+  ,'publicidad_id path_publicidad_imagen fecha_inicio_publicidad fecha_fin_publicidad path_publicidad_webpage tipo titulo descripcion condominio_id').catch((error) => {
   
-if (err) {
-  console.log(err);
-  return res.send(err);
-}
-  console.log(results);
-  return res.send(results);
-}
-
-).sort({'fecha_inicio_publicidad' : -1})
-.limit(10);
-};
+  console.log(error);
+  return res.send(error);
+  }).sort({'fecha_inicio_publicidad' : -1})
+  .limit(10);
+  console.log(findAllPub);
+  return res.send(findAllPub);
+  };
